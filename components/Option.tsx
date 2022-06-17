73d708ee9f3eb112d7f3c4241e5dc624731463b1
ktmihs <yellowgreen423@gmail.com>
2022-06-18 01:11:09 +0900

    design: bodyBudy#8 Modify the style of markup for flexibility
    - Option.tsx

--- components/Option.tsx
+++ components/Option.tsx
@@ -4,16 +4,20 @@ const Option = ({options}:any) => {
   const OptionColors = [
     '#A3D8FA', '#D8CAFF', '#D2EDFF', '#FED6A6', '#FAE9AD'
   ]
+  const OPT_LEN = OptionColors.length;
   
   const OptionList = styled.ul`
-    display: flex;
-    flex-flow: row nowrap;
-    justify-content: center;
-    gap: 6px;
+    width: 70vw;
+    margin-top: 20px;
+    height: fit-content;
   `;
 
   const OptionItem = styled.li`
-    background-color: ${({optColor}:{optColor:number}) => OptionColors[optColor]};
+    white-space: nowrap;
+    display: inline-block;
+    margin-right: 6px;
+    margin-bottom: 6px;
+    background-color: ${({optColor}:{optColor:number}) => OptionColors[optColor % OPT_LEN]};
     border-radius: 5px;
     padding: 4px;
   `;
