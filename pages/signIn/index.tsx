5d33ffe58650c2a8d1d14387168ea8eb371d4522
urther <alswlkku@ajou.ac.kr>
2022-06-20 14:40:35 +0900

    design : #9 Add signIn and OnBoarding markup
    - OnBoarding page Markup
    - signIn page markup

--- pages/signIn/index.tsx
+++ pages/signIn/index.tsx
@@ -1,8 +1,24 @@
+import { SocialLinkButton } from '@components/common/button';
+import Image from 'next/image';
+
+import fullLogo from '/public/assets/fullLogo.svg';
+
 const SignIn = () => {
   return (
-    <section>
+    <>
+      <h1>
+        <span className="srOnly">바디버디</span>
+        <Image
+          src={fullLogo}
+          title="바디버디"
+          alt="바디버디 로고"
+          width={'193.57.px'}
+          height={'132px'}
+        />
+      </h1>
       <span>SNS로 간편하게 로그인 하세요 !</span>
-    </section>
+      <SocialLinkButton NaverLink="/" KakaoLink="/" purpose="signIn" />
+    </>
   );
 };
 
