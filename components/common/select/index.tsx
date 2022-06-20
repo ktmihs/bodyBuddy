e5a22021997d7d17ccd0e2a204bec6736e2a40f9
urther <alswlkku@ajou.ac.kr>
2022-06-20 18:18:00 +0900

    feat : #16 Add select component
    - Add type (select.d.ts)
    - Add select css styling (styledSelect.ts)
    - Add select Component (index.tsx)

---
+++ components/common/select/index.tsx
@@ -0,0 +1,50 @@
+import Image from 'next/image';
+import { useState } from 'react';
+import { StyledSelect, StyledSelectUl, StyledSelectWrapper } from './styledSelect';
+
+import selectTriangle from '@assets/common/selectTriangle.svg';
+import selectTriangleClose from '@assets/common/selectTriangleClose.svg';
+
+export const Select = ({
+  currentSelectedData,
+  selectData,
+  selectWidth,
+  onSetCurrentSelected,
+}: SelectProps) => {
+  const [isListOpen, setIsListOpen] = useState(false);
+
+  const onSetIsListOpen = () => setIsListOpen(!isListOpen);
+
+  const onClickCurrentSelected = (e: React.SyntheticEvent): void => {
+    if (!(e.target instanceof HTMLLIElement)) {
+      return;
+    }
+
+    onSetCurrentSelected(e.target.dataset.id);
+    setIsListOpen(false);
+  };
+
+  return (
+    <StyledSelectWrapper>
+      <StyledSelect onClick={onSetIsListOpen} width={selectWidth}>
+        <span>{currentSelectedData}</span>
+        {isListOpen ? (
+          <Image src={selectTriangleClose} alt="옵션 닫기" width="15px" height="15px" />
+        ) : (
+          <Image src={selectTriangle} alt="옵션 열기" width="15px" height="15px" />
+        )}
+      </StyledSelect>
+      {isListOpen ? (
+        <StyledSelectUl width={selectWidth}>
+          {selectData.map((data) => (
+            <li key={data} data-id={data} onClick={onClickCurrentSelected}>
+              {data}
+            </li>
+          ))}
+        </StyledSelectUl>
+      ) : (
+        <></>
+      )}
+    </StyledSelectWrapper>
+  );
+};
