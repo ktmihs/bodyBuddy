ee1a826d0a44215d62a141f3118a77a38d100012
h-sick <luna23825@gmail.com>
2022-06-21 11:02:13 +0900

    feat: #23 markup chat pages
    - chat room, chat list, metting pages
    - seperate Talk, GymInfo components from chat room

---
+++ components/layout/chat/index.ts
@@ -0,0 +1,2 @@
+export { default as Talk } from './Talk';
+export { default as GymInfo } from './GymInfo';
---
+++ pages/chat/meeting.tsx
@@ -0,0 +1,16 @@
+const Meeting = () => {
+  return (
+    <article>
+      <div>
+        <button></button>
+        <h2>약속 잡기</h2>
+      </div>
+      <section>
+        <span>약속시간</span>
+        <time>2022.06.15 오후 3:30</time>
+      </section>
+    </article>
+  );
+};
+
+export default Meeting;
