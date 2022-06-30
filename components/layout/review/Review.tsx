ff812e73aea19eb991e1cf0fef8c9e6361c8a97d
hann3 <soyo_0131@naver.com>
2022-06-30 12:38:35 +0900

    design: #47 Review Page - My Review List
    - 마이페이지 내에서 나의 리뷰 보기 페이지 디자인
    - 리뷰가 없을 경우/리뷰를 삭제할 경우 디자인 추가

--- components/layout/review/Review.tsx
+++ components/layout/review/Review.tsx
@@ -4,58 +4,128 @@ import ButtonGroup from '@components/common/buttongroup';
 import RatingGroup from '@components/common/rating';
 import { RightButtonModal } from '@components/common/modal';
 import { useState } from 'react';
+import styled from '@emotion/styled';
+import NoContent from '@components/common/noContent';
 
-const Review = () => {
+const ReviewGroup = styled.div`
+  margin-left: 5%;
+  padding: 1%;
+`;
+
+const Reveiw = styled.article`
+  width: 90%;
+  border-top: 1px solid ${({ theme }) => theme.lightGray};
+  border-bottom: 1px solid ${({ theme }) => theme.lightGray};
+  padding: 5% 0;
+  & > div:nth-of-type(1) {
+    display: flex;
+  }
+`;
+
+const RatingContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-content: flex-start;
+  gap: 5px;
+  margin-left: 10px;
+  time,
+  span {
+    font-size: 14px;
+  }
+  div:nth-of-type(1) {
+    flex-grow: 1;
+  }
+`;
+
+const MainText = styled.div`
+  margin: 3% 0;
+  img {
+    border-radius: 15px;
+  }
+  p {
+    margin: 10px 0;
+  }
+`;
+
+const TrainerInfo = styled.div`
+  padding: 15px 0;
+  span {
+    font-size: 14px;
+    margin-right: 15px;
+  }
+
+  span:nth-of-type(2) {
+    background-color: ${({ theme }) => theme.purple};
+    color: white;
+    padding: 2%;
+    border-radius: 15px;
+  }
+`;
+
+const MyReview = () => {
   const [isModalState, onChangeSetState] = useState<boolean>(true);
   return (
-    <div className="reviewList">
-      <article>
+    <ReviewGroup className="reviewList">
+      <Reveiw>
         <div>
-          <Image src="/assets/common/profile.svg" alt="프로필" width="50" height="50" />
-          <PostMetaInfo
-            nickname="루시안"
-            time={new Date()}
-            className="review"
-            displayByDate={true}
-          ></PostMetaInfo>
-          <RatingGroup isEditingMode={false} />
+          <Image src="/assets/common/profile.svg" alt="프로필" width="30" height="30" />
+          <RatingContainer>
+            <PostMetaInfo
+              nickname="루시안"
+              time={new Date()}
+              className="review"
+              displayByDate={true}
+            ></PostMetaInfo>
+            <RatingGroup isEditingMode={false} width={15} height={15} />
+          </RatingContainer>
+          <ButtonGroup className="review" />
         </div>
-        <ButtonGroup className="review" />
-        <div>
-          <span>최세민 트레이너</span>
-          <span>PT | 다이어트</span>
-          <Image src="/assets/common/trainer.jpg" alt="프로필" width="200" height="200" />
+        <MainText>
+          <TrainerInfo>
+            <span>최세민 트레이너</span>
+            <span>PT | 다이어트</span>
+          </TrainerInfo>
+          <Image src="/assets/common/trainer.jpg" alt="프로필" width="100" height="100" />
           <p>제가 만난 최고의 요가 강사님입니다!! 다음에 요가해도 강사님에게 할거에요..</p>
-        </div>
-      </article>
-      <article>
+        </MainText>
+      </Reveiw>
+      <Reveiw>
         <div>
-          <Image src="/assets/common/profile.svg" alt="프로필" width="50" height="50" />
-          <PostMetaInfo
-            nickname="루시안"
-            time={new Date()}
-            className="review"
-            displayByDate={true}
-          ></PostMetaInfo>
-          <RatingGroup isEditingMode={false} />
+          <Image src="/assets/common/profile.svg" alt="프로필" width="30" height="30" />
+          <RatingContainer>
+            <PostMetaInfo
+              nickname="루시안"
+              time={new Date()}
+              className="review"
+              displayByDate={true}
+            ></PostMetaInfo>
+            <RatingGroup isEditingMode={false} width={15} height={15} />
+          </RatingContainer>
+          <ButtonGroup className="review" />
         </div>
-        <ButtonGroup className="review" />
-        <div>
-          <span>이채령 트레이너</span>
-          <span>PT | 다이어트</span>
-          <Image src="/assets/common/trainer.jpg" alt="프로필" width="200" height="200" />
+        <MainText>
+          <TrainerInfo>
+            <span>이채령 트레이너</span>
+            <span>요가 | 다이어트</span>
+          </TrainerInfo>
+          <Image src="/assets/common/trainer.jpg" alt="프로필" width="100" height="100" />
           <p>제가 만난 최고의 요가 강사님입니다!! 다음에 요가해도 강사님에게 할거에요..</p>
-        </div>
-      </article>
+        </MainText>
+      </Reveiw>
+      {/* 
       <RightButtonModal
         modalContent="후기를 삭제하시겠습니까?"
         rightButtonContent="후가 삭제 완료"
         onClickedRightBtn={() => console.log('완료!')}
         isModalState={isModalState}
         onChangeSetState={onChangeSetState}
-      />
-    </div>
+      /> */}
+      {/* <NoContent
+        title="작성한 후기가 없어요!"
+        subTitle="작성한 후기는 트레이너님과 버디들에게 도움이 됩니다."
+      /> */}
+    </ReviewGroup>
   );
 };
 
-export default Review;
+export default MyReview;
--- pages/profile/posts/[id].tsx
+++ pages/profile/posts/[id].tsx
@@ -1,8 +1,23 @@
 import { TitleBar } from '@components/common/title';
-import Review from '@components/layout/review/Review';
+import MyReview from '@components/layout/review/Review';
 import { service } from '@data';
 import { useState } from 'react';
 import { Select } from '@components/common/select';
+import styled from '@emotion/styled';
+
+const ServiceGroup = styled.div`
+  margin-left: 5%;
+  margin-bottom: 5%;
+  display: flex;
+  gap: 20px;
+  & > span:nth-of-type(1) {
+    align-self: center;
+  }
+  span {
+    font-size: 13px;
+    color: ${({ theme }) => theme.black};
+  }
+`;
 
 const PostList = () => {
   const [category, setCategory] = useState('상담');
@@ -11,13 +26,16 @@ const PostList = () => {
     <section>
       <h2 className="srOnly">리뷰 목록</h2>
       <TitleBar left={left} centerTitle={'내가 작성한 리뷰'} />
-      <Select
-        currentSelectedData={category}
-        onSetCurrentSelected={setCategory}
-        selectData={service}
-        selectWidth={100}
-      />
-      <Review />
+      <ServiceGroup>
+        <span>분류</span>
+        <Select
+          currentSelectedData={category}
+          onSetCurrentSelected={setCategory}
+          selectData={service}
+          selectWidth={100}
+        />
+      </ServiceGroup>
+      <MyReview />
     </section>
   );
 };
