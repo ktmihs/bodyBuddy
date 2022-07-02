73a4c17dd8af6b156229682a1f43987b1d3734b2
hann3 <soyo_0131@naver.com>
2022-07-02 22:44:18 +0900

    refactor: Community - Separate Postlist
    - 커뮤니티 게시물 목록에서 게시물 목록을 별도로 분리
    - 배열을 통해 렌더링하도록 수정. 키는 임의로 인덱스로 지정 (수정 예정)

---
+++ components/layout/community/Post.tsx
@@ -0,0 +1,194 @@
+import styled from '@emotion/styled';
+import Image from 'next/image';
+import { LikeAndCommentCount, PostMetaInfo } from '@components/common/meta';
+
+interface PostListProps {
+  selectedItem: string;
+}
+
+const Post = styled.article`
+  position: relative;
+  height: 100px;
+  padding: 3%;
+  margin-left: 10px;
+  margin-top: 5%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  a {
+    display: flex;
+    flex-direction: column;
+    text-decoration: none;
+    width: calc(100% - 120px);
+    height: 80%;
+    &:visited,
+    & {
+      color: inherit;
+    }
+
+    p:nth-of-type(1) {
+      font-weight: bold;
+      font-size: 16px;
+      margin-bottom: 5%;
+    }
+    p:nth-of-type(2) {
+      display: -webkit-box;
+      word-wrap: break-word;
+      -webkit-line-clamp: 3;
+      -webkit-box-orient: vertical;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+  }
+`;
+
+const ImageContainer = styled.div`
+  position: absolute;
+  right: 20px;
+  top: 10px;
+  img {
+    object-fit: cover;
+  }
+`;
+const MetaContainer = styled.div`
+  display: flex;
+  margin-bottom: 0;
+
+  div:nth-of-type(1) {
+    width: 40%;
+    align-self: flex-end;
+  }
+`;
+
+const PostList = ({ selectedItem }: PostListProps) => {
+  const posts = [
+    {
+      content: '요만한건데',
+      creationDate: '2021-06-14T06:30:10.792Z',
+      fieldId: 'QrcJU3p8uWBbL0J6mjNL',
+      image: '/assets/community/blank.svg',
+      title: '트쌤한테 먹을거 줘도 돼?',
+      totalComments: 5,
+      totalLiked: 25,
+      userId: 'ReBwBbk6bhMIcJWPmJCU',
+    },
+    {
+      content: '죽겠어요 원래 이런가요... 덕분에 저는 다리를 잃었고',
+      creationDate: '2021-06-21T06:30:10.792Z',
+      fieldId: 'QrcJU3p8uWBbL0J6mjNL',
+      image: '',
+      title: '트레이너가 하체만 시켜요',
+      totalComments: 3,
+      totalLiked: 1,
+      userId: 'ReBwBbk6bhMIcJWPmJCU',
+    },
+    {
+      content: 'ㅠㅠ',
+      creationDate: '2021-06-27T13:30:10.792Z',
+      fieldId: 'QrcJU3p8uWBbL0J6mjNL',
+      image: '',
+      title: ' 수영하다 기절한 SSUL',
+      totalComments: 3,
+      totalLiked: 0,
+      userId: 'ReBwBbk6bhMIcJWPmJCU',
+    },
+    {
+      content: '사정상 양도합니다',
+      creationDate: '2021-06-30T06:30:10.792Z',
+      fieldId: 'QrcJU3p8uWBbL0J6mjNL',
+      image: '/assets/community/blank.svg',
+      title: 'PT 이용권 10회 팝니다',
+      totalComments: 0,
+      totalLiked: 0,
+      userId: 'ReBwBbk6bhMIcJWPmJCU',
+    },
+    {
+      content:
+        ' 회원들이 힘들어할 때마다 자기는 행복하다면서 ㄷㄷ... 매일 보고 싶대요. 트레이너 자격에 남의 고통 즐기기 같은 게 있는 건가요? ㅡㅡ',
+      creationDate: '2021-07-01T12:00:10.792Z',
+      fieldId: 'QrcJU3p8uWBbL0J6mjNL',
+      image: '/assets/community/blank.svg',
+      title: '트레이너가 이상해요',
+      totalComments: 5,
+      totalLiked: 8,
+      userId: 'ReBwBbk6bhMIcJWPmJCU',
+    },
+    {
+      content: '사정상 양도합니다',
+      creationDate: '2021-07-01T18:00:10.792Z',
+      fieldId: 'QrcJU3p8uWBbL0J6mjNL',
+      image: '/assets/community/blank.svg',
+      title: 'PT 이용권 10회 팝니다',
+      totalComments: 0,
+      totalLiked: 0,
+      userId: 'ReBwBbk6bhMIcJWPmJCU',
+    },
+
+    {
+      content:
+        ' 회원들이 힘들어할 때마다 자기는 행복하다면서 ㄷㄷ... 매일 보고 싶대요. 트레이너 자격에 남의 고통 즐기기 같은 게 있는 건가요? ㅡㅡ',
+      creationDate: '2021-07-02T15:00:10.792Z',
+      fieldId: 'QrcJU3p8uWBbL0J6mjNL',
+      image: '/assets/community/blank.svg',
+      title: '트레이너가 이상해요',
+      totalComments: 5,
+      totalLiked: 8,
+      userId: 'ReBwBbk6bhMIcJWPmJCU',
+    },
+    {
+      content: '야무지게 먹어야징',
+      creationDate: '2021-07-02T16:00:10.792Z',
+      fieldId: 'QrcJU3p8uWBbL0J6mjNL',
+      image: '',
+      title: '치팅메뉴 추천받아요!',
+      totalComments: 2,
+      totalLiked: 0,
+      userId: 'ReBwBbk6bhMIcJWPmJCU',
+    },
+    {
+      content:
+        ' 회원들이 힘들어할 때마다 자기는 행복하다면서 ㄷㄷ... 매일 보고 싶대요. 트레이너 자격에 남의 고통 즐기기 같은 게 있는 건가요? ㅡㅡ',
+      creationDate: '2021-07-02T16:00:10.792Z',
+      fieldId: 'QrcJU3p8uWBbL0J6mjNL',
+      image: '/assets/community/blank.svg',
+      title: '트레이너가 이상해요',
+      totalComments: 5,
+      totalLiked: 8,
+      userId: 'ReBwBbk6bhMIcJWPmJCU',
+    },
+  ];
+  return (
+    <div>
+      {posts.map((post, index) => (
+        <Post key={index}>
+          <a href="community/1">
+            <p>{post.title}</p>
+            <p>{post.content}</p>
+
+            {post.image ? (
+              <ImageContainer>
+                <Image src={post.image} alt="첨부한 사진" width="100" height="80" />
+              </ImageContainer>
+            ) : (
+              ''
+            )}
+          </a>
+          <MetaContainer>
+            <PostMetaInfo
+              nickname="울면근손실"
+              time={new Date(post.creationDate)}
+              className="list"
+            />
+            <LikeAndCommentCount
+              like={post.totalLiked}
+              comment={post.totalComments}
+              isClicked={true}
+            />
+          </MetaContainer>
+        </Post>
+      ))}
+    </div>
+  );
+};
+
+export default PostList;
--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -3,66 +3,12 @@ import { TopButton } from '@components/common/button';
 import type { NextPage } from 'next';
 import Image from 'next/image';
 import styled from '@emotion/styled';
-import { LikeAndCommentCount, PostMetaInfo } from '@components/common/meta';
 import { useState } from 'react';
+import PostList from '@components/layout/community/Post';
 
 const CommunityPage = styled.section`
   &:nth-of-type(1) {
     padding-top: 15%;
-  }
-`;
-
-const Post = styled.article`
-  position: relative;
-  height: 100px;
-  padding: 3%;
-  margin-left: 10px;
-  margin-top: 5%;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  a {
-    display: flex;
-    flex-direction: column;
-    text-decoration: none;
-    width: calc(100% - 120px);
-    height: 80%;
-    &:visited,
-    & {
-      color: inherit;
-    }
-
-    p:nth-of-type(1) {
-      font-weight: bold;
-      font-size: 16px;
-      margin-bottom: 5%;
-    }
-    p:nth-of-type(2) {
-      display: -webkit-box;
-      word-wrap: break-word;
-      -webkit-line-clamp: 3;
-      -webkit-box-orient: vertical;
-      overflow: hidden;
-      text-overflow: ellipsis;
-    }
-  }
-`;
-
-const ImageContainer = styled.div`
-  position: absolute;
-  right: 20px;
-  top: 10px;
-  img {
-    object-fit: cover;
-  }
-`;
-const MetaContainer = styled.div`
-  display: flex;
-  margin-bottom: 0;
-
-  div:nth-of-type(1) {
-    width: 40%;
-    align-self: flex-end;
   }
 `;
 
@@ -108,140 +54,7 @@ const Community: NextPage = () => {
     <CommunityPage>
       <h2 className="srOnly">커뮤니티 게시판</h2>
       <ItemGroup changeSelectedItem={changeSelectedItem} />
-      <div>
-        <Post>
-          <a href="community/1">
-            <p>트레이너가 하체만 시켜요</p>
-            <p>죽겠어요 원래 이런가요... 덕분에 저는 다리를 잃었고</p>
-            <ImageContainer>
-              <Image src="/assets/community/blank.svg" alt="첨부한 사진" width="100" height="80" />
-            </ImageContainer>
-          </a>
-          <MetaContainer>
-            <PostMetaInfo
-              nickname="울면근손실"
-              time={new Date()}
-              className="list"
-              displayByDate={false}
-            />
-            <LikeAndCommentCount like={1} comment={3} isClicked={true} />
-          </MetaContainer>
-        </Post>
-        <Post>
-          <a href="community/2">
-            <p>수영하다 기절한 SSUL</p>
-            <p>ㅠㅠ</p>
-          </a>
-          <MetaContainer>
-            <PostMetaInfo
-              nickname="밍망디"
-              time={new Date()}
-              className="list"
-              displayByDate={false}
-            />
-            <LikeAndCommentCount like={1} comment={3} isClicked={false} />
-          </MetaContainer>
-        </Post>
-        <Post>
-          <a href="community/3">
-            <p>PT 이용권 10회 팝니다</p>
-            <p>사정상 양도합니다</p>
-            <ImageContainer>
-              <Image src="/assets/community/blank.svg" alt="첨부한 사진" width="100" height="80" />
-            </ImageContainer>
-          </a>
-          <MetaContainer>
-            <PostMetaInfo
-              nickname="육회랑연어"
-              time={new Date()}
-              className="list"
-              displayByDate={false}
-            />
-            <LikeAndCommentCount like={0} comment={0} isClicked={false} />
-          </MetaContainer>
-        </Post>
-        <Post>
-          <a href="community/4">
-            <p>수영하다 기절한 SSUL</p>
-            <p>ㅠㅠ</p>
-          </a>
-          <MetaContainer>
-            <PostMetaInfo
-              nickname="밍망디"
-              time={new Date()}
-              className="list"
-              displayByDate={false}
-            />
-            <LikeAndCommentCount like={1} comment={3} isClicked={true} />
-          </MetaContainer>
-        </Post>
-        <Post>
-          <a href="community/5">
-            <p>트레이너가 이상해요</p>
-            <p>
-              회원들이 힘들어할 때마다 자기는 행복하다면서 ㄷㄷ... 매일 보고 싶대요. 트레이너 자격에
-              남의 고통 즐기기 같은 게 있는 건가요? ㅡㅡ;
-            </p>
-          </a>
-          <MetaContainer>
-            <PostMetaInfo
-              nickname="삐약이"
-              time={new Date()}
-              className="list"
-              displayByDate={false}
-            />
-            <LikeAndCommentCount like={5} comment={3} isClicked={true} />
-          </MetaContainer>
-        </Post>
-        <Post>
-          <a href="community/6">
-            <p>치팅데이 메뉴 추천받아요</p>
-            <p>마구 먹어야지~!</p>
-          </a>
-          <MetaContainer>
-            <PostMetaInfo
-              nickname="돼지KIN"
-              time={new Date()}
-              className="list"
-              displayByDate={false}
-            />
-            <LikeAndCommentCount like={0} comment={5} isClicked={false} />
-          </MetaContainer>
-        </Post>
-        <Post>
-          <a href="community/7">
-            <p>데드리프트..봐주세요</p>
-            <p>데드리프트 할 때마다 허리가 아픈데 이게 맞는지 모르겠어요 ㅜㅜ</p>
-            <ImageContainer>
-              <Image src="/assets/community/blank.svg" alt="첨부한 사진" width="100" height="80" />
-            </ImageContainer>
-          </a>
-          <MetaContainer>
-            <PostMetaInfo
-              nickname="먹고싶당"
-              time={new Date()}
-              className="list"
-              displayByDate={false}
-            />
-            <LikeAndCommentCount like={0} comment={3} isClicked={false} />
-          </MetaContainer>
-        </Post>
-        <Post>
-          <a href="community/8">
-            <p>상여자특:비와도 운동감</p>
-            <p>저는 하여자입니다</p>
-          </a>
-          <MetaContainer>
-            <PostMetaInfo
-              nickname="맘스터치"
-              time={new Date()}
-              className="list"
-              displayByDate={false}
-            />
-            <LikeAndCommentCount like={4} comment={1} isClicked={false} />
-          </MetaContainer>
-        </Post>
-      </div>
+      <PostList selectedItem={selectedItem} />
       <PostButton>
         <a href="community/posting">
           <Image src="/assets/community/pencil.svg" alt="글쓰기" width={15} height={15}></Image>
