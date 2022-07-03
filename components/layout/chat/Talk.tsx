434e067e0ee909e469c72cade4be817888a5f1f6
h-sick <luna23825@gmail.com>
2022-07-03 17:51:20 +0900

    feat: styling chatting pages
    - chat, chat list, meeting
    - add icons
    - seperate TrainerInfo as component

--- components/layout/chat/Talk.tsx
+++ components/layout/chat/Talk.tsx
@@ -1,3 +1,43 @@
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  position: relative;
+  margin-bottom: 30px;
+`;
+
+const Message = styled.div<Pick<TalkProps, 'type'>>`
+  width: 216px;
+  padding: 12px 14px;
+  margin-bottom: 5px;
+  border-radius: 10px;
+  font-size: 13px;
+  line-height: 17px;
+  margin-left: ${({ type }) => (type === 'trainer' ? '0px' : 'auto')};
+  margin-right: ${({ type }) => (type === 'trainer' ? 'auto' : '0px')};
+  color: ${({ type, theme }) => (type === 'me' ? theme.white : theme.black)};
+  background-color: ${({ type, theme }) =>
+    type === 'me' ? theme.purple : type === 'trainer' ? theme.lightGray : theme.white};
+`;
+
+const Time = styled.time<Pick<TalkProps, 'type'>>`
+  color: ${({ theme }) => theme.mediumGray};
+  font-size: 10px;
+  float: ${({ type }) => (type === 'trainer' ? 'left' : 'right')};
+`;
+
+const ReviewButton = styled.button`
+  position: absolute;
+  right: 0;
+  bottom: -40px;
+  color: ${({ theme }) => theme.white};
+  background-color: ${({ theme }) => theme.lightBlue};
+  font-size: 10px;
+  padding: 5px 8px;
+  border: none;
+  border-radius: 10px;
+  float: right;
+`;
+
 interface TalkProps {
   content: string;
   time: string;
@@ -6,11 +46,11 @@ interface TalkProps {
 
 const Talk = ({ content, time, type }: TalkProps) => {
   return (
-    <>
-      <div>{content}</div>
-      <time>{time}</time>
-      {type === 'info' && <button>후기 남기기</button>}
-    </>
+    <Container>
+      <Message type={type}>{content}</Message>
+      <Time type={type}>{time}</Time>
+      {type === 'info' && <ReviewButton>후기 남기기</ReviewButton>}
+    </Container>
   );
 };
 
