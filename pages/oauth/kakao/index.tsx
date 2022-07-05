cf0eef61a64253a58d4d91c462c00121d7cf60a6
urther <alswlkku@ajou.ac.kr>
2022-07-06 00:29:20 +0900

    feat : #57 Add kakao Login feature
    - add kakao login feature

--- pages/oauth/kakao/index.tsx
+++ pages/oauth/kakao/index.tsx
@@ -1,11 +1,26 @@
+import styled from '@emotion/styled';
 import axios from 'axios';
 import Router from 'next/router';
 
 import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { loginEmail } from 'redux/userSlice';
+
+import { TailSpin } from 'react-loader-spinner';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 
 const REDIRECT_URI = 'http://localhost:3000/oauth/kakao';
 
+const SpinContainer = styled.div`
+  position: absolute;
+  top: 30%;
+  left: 50%;
+  transform: translateX(-50%);
+`;
+
 const KakaoLogin = () => {
+  const dispatch = useDispatch();
+
   const getToken = async (code: string | null) => {
     try {
       const {
@@ -38,8 +53,14 @@ const KakaoLogin = () => {
 
       if (data.result) {
         // 회원 데이터가 있는 경우
+        Router.push('/');
       } else {
         // 회원 데이터가 없는 경우
+        const payload = {
+          email: data.userInfo.email,
+          gender: data.userInfo.gender === 'female' ? false : true,
+        };
+        dispatch(loginEmail(payload));
         Router.push('/signUp/checkCategory');
       }
     } catch (e) {
@@ -51,7 +72,12 @@ const KakaoLogin = () => {
     const code = new URL(window.location.href).searchParams.get('code');
     getToken(code);
   }, []);
-  return;
+
+  return (
+    <SpinContainer>
+      <TailSpin height="70" width="70" color="#858FF1" ariaLabel="loading" />
+    </SpinContainer>
+  );
 };
 
 export default KakaoLogin;
