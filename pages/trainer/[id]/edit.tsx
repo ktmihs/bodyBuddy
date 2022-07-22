c93725d223a19a34f70d54ad29b7e3b8ccb6580b
ktmihs <yellowgreen423@gmail.com>
2022-07-22 21:49:02 +0900

    feat: Get trainer information
    - trainer/edit.tsx

--- pages/trainer/[id]/edit.tsx
+++ pages/trainer/[id]/edit.tsx
@@ -1,48 +1,55 @@
+import { getTrainerData } from '@api/firebase';
 import { FixedBottomButton } from '@components/common/button';
 import { TitleBar } from '@components/common/title';
 import { Edit } from '@components/layout/trainer/edit';
 import styled from '@emotion/styled';
 import Link from 'next/link';
-import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 const TrainerEdit = () => {
-  // trainer 정보 받아와서 초기값 저장
-  const id = 1;
-  const field = 'PT';
-  const purpose = '다이어트';
-  const images = ['/assets/common/profile.svg', '/assets/common/profile.svg'];
-  if (images.length < 3) for (let i = 0; i < 3 - images.length; i++) images.push('');
-  const gymImage = '/assets/common/profile.svg';
-  const career = [
-    {
-      id: '1234',
-      content:
-        '숨쉬는 경력은 경력이 아닌가요??숨쉬는 경력은 경력이 아닌가요??숨쉬는 경력은 경력이 아닌가요??',
-      image: '/assets/common/star.svg',
-      isApproval: false,
-    },
-    {
-      id: '1235',
-      content: '숨쉬는 경력은 경력이 아닌가요',
-      image: '/assets/common/star.svg',
-      isApproval: true,
-    },
-    {
-      id: '1236',
-      content: '숨쉬는 경력은 경력이 아닌가요',
-      image: '/assets/common/star.svg',
-      isApproval: false,
-    },
-  ];
-  const getCost = '10000';
+  const router = useRouter();
+  const id = router.query.id;
 
-  const [newField, setNewField] = useState(field || '');
-  const [newPurpose, setNewPurpose] = useState(purpose || '');
-  const [profileUrl, setProfileUrl] = useState<string>(images[0]);
-  const [imagesUrl, setImagesUrl] = useState<string[]>(images);
-  const [gymUrl, setGymUrl] = useState<string[]>([gymImage || '']);
-  const [careers, setCareers] = useState<CareerProps[]>(career || []);
-  const [cost, setCost] = useState<string>(getCost || '10000');
+  const [newField, setNewField] = useState<string>();
+  const [newPurpose, setNewPurpose] = useState<string>();
+  const [address, setNewAddress] = useState<string>();
+  const [introdution, setIntrodution] = useState<string>();
+  const [isOnine, setIsOnine] = useState<boolean>();
+  const [profileUrl, setProfileUrl] = useState<string[]>();
+  const [imagesUrl, setImagesUrl] = useState<string[]>();
+  const [gymUrl, setGymUrl] = useState<string[]>();
+  const [careers, setCareers] = useState<CareerProps[]>();
+  const [cost, setCost] = useState<string>();
+
+  useEffect(() => {
+    // 트레이너 정보 받아오기
+    if (typeof id === 'string') {
+      const data = getTrainerData(id);
+      data.then((res) => {
+        if (res?.length) {
+          const {
+            field,
+            purpose,
+            address,
+            introduction,
+            isOnline,
+            images,
+            gymImage,
+            careers,
+            price,
+          } = res[0];
+          setNewField(field);
+          setNewPurpose(purpose);
+          setProfileUrl(images[0]);
+          setImagesUrl(images);
+          setGymUrl([gymImage]);
+          setCareers(careers);
+          setCost(price);
+        }
+      });
+    }
+  }, [id]);
 
   const trainerState = {
     field: newField,
@@ -97,7 +104,7 @@ const TrainerEdit = () => {
     cursor: pointer;
   `;
 
-  return (
+  return newField ? (
     <>
       <h2 className="srOnly">내 정보</h2>
       <TitleBar left={left} right={right} centerTitle={'내 정보'} />
@@ -112,6 +119,8 @@ const TrainerEdit = () => {
         buttonTitle={'변경 사항 저장'}
       />
     </>
+  ) : (
+    <></>
   );
 };
 
